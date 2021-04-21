63e37832b1557bb75c543905f9ff36c361e2eb0f
Raden-Daffa <radendaffa.siswa@smkn4bdg.sch.id>
2021-04-22 02:36:58 +0700

    invoice

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -1,21 +1,63 @@
-@extends('layouts.admin')
+<!DOCTYPE html>
+<html lang="en">
 
-@section('content')
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Product Types</h1>
-            </div>
-            <div class="col-sm-6">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Marimasak</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('lte/plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <!-- iCheck -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/jqvmap/jqvmap.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href=" {{ asset('lte/dist/css/adminlte.min.css')}}">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/daterangepicker/daterangepicker.css')}}">
+    <!-- summernote -->
+    <link rel="stylesheet" href=" {{ asset('lte/plugins/summernote/summernote-bs4.min.css')}}">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+
+
+        <!-- Navbar -->
+        @include('./component/header')
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        @include('./component/sidebar')
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                <h1 class="m-0">Invoice</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="/">Home</a></li>
-                    <li class="breadcrumb-item active">Product Types</li>
+                    <li class="breadcrumb-item active">Invoice</li>
                 </ol>
-            </div>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
         </div>
-    </div>
-</div>
+        <!-- /.content-header -->
 
 <section class="content">
     <div class="container-fluid">
@@ -31,42 +73,73 @@
                         </div>
                     @endif
                     <div class="card-header">
-                        <h3 class="card-title">List Product Types</h3>
+                        <h3 class="card-title">List Invoice</h3>
                         <div class="card-tools">
-                            <form method="get" role="form" class="input-group input-group-sm" style="width: 200px;">
-                                <div class="input-group-append">
-                                    <button type="reset" class="btn btn-default" onclick="clearSearch()">
-                                        X
-                                    </button>
-                                </div>
-                                <input
-                                    type="text"
-                                    name="search"
-                                    value="{{ request()->get('name') }}"
-                                    class="form-control float-right"
-                                    placeholder="Search"
-                                >
-                                <div class="input-group-append">
-                                    <button type="submit" class="btn btn-default">
-                                    <i class="fas fa-search"></i>
-                                    </button>
-                                </div>
-                                <a href="{{ route('product-types.create') }}">
+                            <div class="input-group input-group-sm" style="width: 200px;">
+                            <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-default">
+                                <i class="fas fa-search"></i>
+                                </button>
+                            </div> 
+                                <a href="">
                                     <i class="fas fa-plus m-2 ml-4 mr-4"></i>
                                 </a>
-                            </form>
+                            </div>
                         </div>
-                    </div>
-                    <div class="card-body table-responsive p-0">
-                        <table class="table table-hover table-bordered text-nowrap">
+                        </div>
+                        <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
                             <thead>
-                                <tr>
-                                    <th class="text-center" style="width:10%;">ID</th>
-                                    <th class="text-center" style="width:70%;">nama</th>
-                                    <th class="text-center" style="width:20%;">Aksi</th>
-                                </tr>
+                            <tr>
+                                <th>Invoice ID</th>
+                                <th>Nama</th>
+                                <th>Tanggal</th>
+                                <th>Status</th>
+                                <th>Total Tagihan</th>
+                                <th>#</th>
+                            </tr>
                             </thead>
                             <tbody>
+                                <tr>
+                                    <td>00001</td>
+                                    <td>Peter Lee</td>
+                                    <td>17-04-2021</td>
+                                    <td>Dibayar</td>
+                                    <td>Rp 1.500.000</td>
+                                    <td> 
+                                    <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                                    <a href="/edit-role" class="btn btn-warning btn-sm">
+                                    <i class="fas fa-pen "></i>
+                                    </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>00002</td>
+                                    <td>Rain Paul</td>
+                                    <td>21-04-2021</td>
+                                    <td>Dibayar</td>
+                                    <td>Rp 5.770.000</td>
+                                    <td> 
+                                    <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                                    <a href="/edit-role" class="btn btn-warning btn-sm">
+                                    <i class="fas fa-pen "></i>
+                                    </a>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>00003</td>
+                                    <td>Jack Law</td>
+                                    <td>20-03-2021</td>
+                                    <td>Belum Bayar</td>
+                                    <td>Rp 8.930.000</td>
+                                    <td> 
+                                    <button type="submit" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></button>
+                                    <a href="/edit-role" class="btn btn-warning btn-sm">
+                                    <i class="fas fa-pen "></i>
+                                    </a>
+                                    </td>
+                                </tr>
                             </tbody>
                         </table>
                     </div>
@@ -75,42 +148,18 @@
         </div>
     </div>
 </section>
-@endsection
+<!-- /.content -->
+</div>
+        <!-- /.content-wrapper -->
 
-<script>
-    function clearSearch() {
-        var uri = window.location.toString();
-        if (uri.indexOf("?") > 0) {
-            var clean_uri = uri.substring(0, uri.indexOf("?"));
-            window.history.replaceState({}, document.title, clean_uri);
-            window.location = "{{ route('product-types.index') }}";
-        }
-    }
+        @include('./component/footer')
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+        </aside>
+        <!-- /.control-sidebar -->
+    </div>
+    <!-- ./wrapper -->
 
-    function deleteProductType(id) {
-        Swal.fire({
-            title: 'Are you sure?',
-            text: 'You will delete this',
-            icon: 'warning',
-            confirmButtonText: 'Cool',
-            showCancelButton: true,
-        }).then(function(confirm) {
-            if(confirm.isConfirmed) {
-                swal.fire({
-                    title: "Delete Successfully",
-                    message: "You canceled to delete this",
-                    icon: 'success',
-                    time: 200
-                }).then(function() {
-                    $(`#productType-delete-${id}`).submit();
-                })
-            } else {
-                swal.fire(
-                    "Canceled",
-                    "You canceled to delete this",
-                    "error"
-                );
-            }
-        })
-    }
-</script>
+
+</html>
